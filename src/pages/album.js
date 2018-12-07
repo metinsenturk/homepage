c51dc0b91020db24e07a4808adad64fa8f020ad8
Metin Senturk <metinsenturk@me.com>
2018-12-07 00:06:03 -0500

    album page 1st draft

--- src/pages/album.js
+++ src/pages/album.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
 import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout/layout'
 
 class Album extends Component {
   render() {
-    let albums = this.props.data.albums.edges
+    const albums = this.props.data.albums.edges
+    const images = this.props.data.images.edges
+
     return (
       <Layout>
-        {albums.map(({ node }, index) => (
-          <div key={index}>
-            <Link to={`album${node.slug}`}>
-              <h3>{node.title}</h3>
-            </Link>
-            <p>{node.description}</p>
-          </div>
-        ))}
+        {albums.map(({ node }, index) => {
+          const album = node
+          const album_images = images.filter(
+            ({ node }) =>
+              album.slug.replace(/[/]/g, '') === node.relativeDirectory
+          )
+
+          return (
+            <div key={index}>
+              <Link to={`album${album.slug}`}>
+                <h3>{album.title}</h3>
+              </Link>
+              <p>{album.description}</p>
+              <ul style={{listStyle: "none"}}>
+                {album_images.map(({ node }, index) => {
+                  const image = node
+                  return <li style={{display: "inline", padding: "5px"}}><Img key={index} fixed={image.childImageSharp.fixed} /></li>
+                })}
+
+              </ul>
+            </div>
+          )
+        })}
       </Layout>
     )
   }
@@ -31,6 +49,24 @@ export const query = graphql`
           title
           description
           slug
+        }
+      }
+    }
+    images: allFile(
+      filter: {
+        extension: { regex: "/(png|jpeg|jpg)/" }
+        sourceInstanceName: { eq: "album" }
+      }
+    ) {
+      edges {
+        node {
+          relativePath
+          relativeDirectory
+          childImageSharp {
+            fixed(height: 50) {
+              ...GatsbyImageSharpFixed
+            }
+          }
         }
       }
     }
