d83a45e5928faffd51ea14110394fcb6ab5ffed8
Metin Senturk <metinsenturk@me.com>
2019-05-14 02:34:35 -0400

    seo added to pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,33 +4,39 @@ import Img from 'gatsby-image'
 import { Heading, Box, Text, Markdown, Paragraph } from 'grommet'
 import { Image } from 'grommet'
 import Layout from '../components/layout/layout'
+import SEO from '../components/seo/seo';
 
 export default ({ data }) => {
-    const about = data.about.edges[0].node
-    //console.log(about)
+  const about = data.about.edges[0].node
+  //console.log(about)
 
-    const overrides = {
-        img: { component: Image, props: { fit: "cover" } },
-        pre: { props: { size: "large" } },
-        p: { component: Paragraph, props: { size: "large" } }
-    }
+  const overrides = {
+    img: { component: Image, props: { fit: "cover" } },
+    pre: { props: { size: "large" } },
+    p: { component: Paragraph, props: { size: "large" } }
+  }
 
-    return (
-        <Layout>
-            <Box basis="large">
-                <Box>
-                    <Img fluid={about.frontmatter.cover.childImageSharp.fluid} />
-                </Box>
-                <Box gap="medium" elevation="xsmall" pad={{ horizontal: "medium", vertical: "xsmall" }}>
-                    <Heading> {about.frontmatter.title}</Heading>
-                    <Text>{about.frontmatter.description}</Text>
-                    <Markdown components={ overrides }>
-                        {about.rawMarkdownBody}
-                    </Markdown>
-                </Box>
-            </Box>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <SEO
+        pathname="/about/"
+        title="About myself, written by myself."
+        desc=""
+      />
+      <Box basis="large">
+        <Box>
+          <Img fluid={about.frontmatter.cover.childImageSharp.fluid} />
+        </Box>
+        <Box gap="medium" elevation="xsmall" pad={{ horizontal: "medium", vertical: "xsmall" }}>
+          <Heading> {about.frontmatter.title}</Heading>
+          <Text>{about.frontmatter.description}</Text>
+          <Markdown components={overrides}>
+            {about.rawMarkdownBody}
+          </Markdown>
+        </Box>
+      </Box>
+    </Layout>
+  )
 }
 
 export const query = graphql`
